9869ecdc8a4197376d41962307fd898db6926273
deviprajwala <deviprajwala3@gmail.com>
2020-06-15 17:39:24 +0530

    almost done

---
+++ single_link_clus.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+float matrix[100][100];
+//the program deals with the hierarchical clustering with the help of bottom up approach.The method is used here is single link
+
+void distance(float x[100],float y[100],int n);
+float min(float a,float b);
+void print_matrix(int n);
+void min_of_matrix(int n);
+void reduce(int a,int b,int n);
+void cluster(int n);
+
+void distance(float x[100],float y[100],int n)
+{
+ int i,j;
+ float a;
+ for(i=1;i<=n;i++)
+ {
+  for(j=1;j<=n;j++)
+  {
+    a=((x[i]-x[j])*(x[i]-x[j]))+((y[i]-y[j])*(y[i]-y[j]));
+    a=sqrt(a);
+    matrix[i][j]=a;
+    //printf("%.2f ",matrix[i][j]);
+  }
+ }
+}
+
+float min(float a,float b)
+{
+ if(a<b)
+ {
+  return a;
+ }
+ else
+ {
+  return b;
+ }
+}
+void print_matrix(int n)
+{
+ int i,j;
+ for(i=1;i<=n;i++)
+ {
+  for(j=1;j<=n;j++)
+  {
+    printf("%.2f ",matrix[i][j]);
+  }
+  printf("\n");
+ }
+}
+
+void min_of_matrix(int n)
+{
+ int i,j,min_i,min_j;
+ float min;
+ min=9999;
+ for(i=1;i<=n;i++)
+ {
+  for(j=1;j<=n;j++)
+  {
+   if(matrix[i][j]<min && matrix[i][j]!=0)
+   {
+    min=matrix[i][j];
+    min_i=i;
+    min_j=j;
+   }
+  }
+ }
+ printf("\n\nThe rows %d %d  from above matrix are clustered\n",min_i,min_j);
+ reduce(min_i,min_j,n);
+}
+
+void reduce(int a,int b,int n)
+{
+  int i,j;
+  for(i=1;i<=n;i++)
+  {
+  for(j=1;j<=n;j++)
+   {
+     if(i==a&&j==b||i==b&&j==a)
+     {
+       //the values are not changed for above condition
+     }
+     else if(i==a||i==b)
+     {
+       matrix[i][j]=min(matrix[a][j],matrix[b][j]);
+     }
+     else if(j==a||j==b)
+     {
+       matrix[i][j]=min(matrix[i][a],matrix[i][b]);
+     }
+   }
+  }
+}
+
+void cluster(int n)
+{
+ if (n==1)
+ {
+  //print_matrix(n);
+  return;
+ }
+ else
+ {
+  min_of_matrix(n);
+  n--;
+  print_matrix(n);
+  cluster(n);
+ }
+}
+int main()
+{
+
+int i,j,dim;
+float x[100],y[100];
+//declaring the variables required
+
+//printf("Enter the number of data points:");
+scanf("%d",&dim);
+
+//printf("Enter the cordinates\n");
+
+for(i=1;i<=dim;i++)
+{
+// printf("x:");
+ scanf("%f",&x[i]);
+// printf("y:");
+ scanf("%f",&y[i]);
+ //printf("hi");
+}
+//getting the input from the user for further calculation
+
+distance(x,y,dim);
+printf("The distance matrix obtained is\n");
+print_matrix(dim);
+cluster(dim);
+return 0;
+}
